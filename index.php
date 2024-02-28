bd92f41f1e34eccc4ed05d1db0381bc136f4e1fa
Jushua <Pabelicjush@gmail.com>
2024-02-29 07:53:03 +0800

    3rd commit

--- index.php
+++ index.php
@@ -6,16 +6,19 @@
     <title>Task Management System</title>
     <link rel="stylesheet" href="styles.css">
 </head>
+
 <style> 
     </style> 
 </head> 
 <body>
-    <div>
+    <div class="container">
+        <div class="glass-container">
         <h1>Task Management System</h1>
             <ul>
             <h3><li><a href="view_task.php">View Tasks</a></li><h3>
             <h3><li><a href="create_task.php">Add Task</a></li><h3>
             </ul>
+        </div>
     </div>
 </body>
 </html
